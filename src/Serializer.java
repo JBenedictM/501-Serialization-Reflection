efcc771f762b26cdd76d8b8cb73a8346efcba31c
Ben <biller010@gmail.com>
2019-11-04 17:20:38 -0700

    Initial Project Dump

---
+++ src/Serializer.java
@@ -0,0 +1,16 @@
+
+public class Serializer {
+	
+	public org.jdom2.Document serialize(Object obj) {
+		org.jdom2.Document result = null;
+		
+		return result;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+	}
+
+}
